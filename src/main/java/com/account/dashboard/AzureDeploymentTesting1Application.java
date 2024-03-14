d86ebc3dec7aaf29cb32502326aa0044c0c11ab0
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-03-14 13:16:52 +0530

    changes

--- src/main/java/com/account/dashboard/AzureDeploymentTesting1Application.java
+++ src/main/java/com/account/dashboard/AzureDeploymentTesting1Application.java
@@ -1,10 +1,22 @@
 package com.account.dashboard;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class AzureDeploymentTesting1Application {
+	
+	@GetMapping("getAllRemarks")
+	public String  getAllRemarks(){
+		System.out.println("Deployment Testing Has Been Done");
+		return "Deployment Testing Has Been Done";
+	}
+
 
 	public static void main(String[] args) {
 		System.out.println("Testing Aryan");
