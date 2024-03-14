613b6564bb4bf8099ab69634e145a2c7980d14b4
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2024-03-14 12:59:00 +0530

    implements Azure Changes

---
+++ src/main/java/com/account/dashboard/AzureDeploymentTesting1Application.java
@@ -0,0 +1,14 @@
+package com.account.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDeploymentTesting1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Aryan");
+		SpringApplication.run(AzureDeploymentTesting1Application.class, args);
+	}
+
+}
